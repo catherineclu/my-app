ced6d9892a3c925ca064cc553c3507afa3e04cca
Clarice Kim <claricekim@Clarices-MacBook-Air.local>
2023-06-19 22:03:16 -0500

    added navigation and screens

--- App.js
+++ App.js
@@ -1,4 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { StyleSheet, Text, View } from 'react-native';
+import { HomeScreen } from './screens/home';
+import {ItemScreen} from './screens/item';
+
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ItemScreen" component={ItemScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export default MyStack;
+
+/*import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -20,4 +52,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
--- screens/home.jsx
+++ screens/home.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-export const ItemScreen = ({navigation}) => {
+export const HomeScreen = ({navigation}) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
-                Welcome to item page!
+                Home Screen!
             </Text>
         </View>
     );
