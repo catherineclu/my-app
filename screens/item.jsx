a5c5b96e09087c32be9395df32561380e3ee14b9
Clarice Kim <claricekim@Clarices-MacBook-Air.local>
2023-06-19 22:51:17 -0500

    added image

--- screens/item.jsx
+++ screens/item.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider, Box, ScrollView, Text, View, Image} from "native-base";
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export const ItemScreen = ({navigation}) => {
     return (
         <NativeBaseProvider>
-        {/* <View style={styles.container}> */}
-            <Box>Welcome to item page</Box>
-        {/* </View> */}
+        <Box safeArea flex={1} bg={Colors.white}>Welcome to item page
+            <ScrollView px={5} showsVerticalScrollIndicator={false}>
+                <Image source={require("../assets/images/cucumber.jpg")} 
+                        alt="cucumber image" 
+                        w="full" 
+                        h={300} 
+                        resizeMode="contain"
+                />
+            </ScrollView>
+        <Text>Vendor name: The Cucumber Chef</Text>
+        <Text>Cost, Location</Text>
+        <Text>Menu coming below lol</Text>
+        </Box>
         </NativeBaseProvider>
     );
 }
