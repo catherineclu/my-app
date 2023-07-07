fead7f09f66da761e1f35593c1fdd029977adf18
Clarice Kim <claricekim@Clarices-MacBook-Air.local>
2023-07-07 14:58:05 -0500

    implemented UI

--- screens/logins.jsx
+++ screens/logins.jsx
@@ -1,6 +1,6 @@
-import { KeyboardAvoidingView } from 'native-base';
 import React from 'react';
-import { KeyboardAvoidingView, View, Text, TextInput, Button, Image, SafeAreaView, StyleSheet, Dimensions, Pressable, TextInput, TouchableOpacity } from 'react-native';
+import { KeyboardAvoidingView, View, Text, TextInput, Button, Image, SafeAreaView, StyleSheet, Dimensions, Pressable, TouchableOpacity } from 'react-native';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 import styles from '../style.js'
 
 export const LoginScreen = ({navigation}) => {
@@ -33,12 +33,12 @@ export const LoginScreen = ({navigation}) => {
                     <TouchableOpacity
                         onPress={() => { }}
                         style={loginstyles.button}>
-                        <Text style={loginstyles.button}>Login</Text>
+                        <Text style={loginstyles.buttonText}>Login</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         onPress={() => { }}
                         style={[loginstyles.button, loginstyles.buttonOutline]}>
-                        <Text styles={loginstyles.buttonOutlineText} >Login</Text>
+                        <Text style={loginstyles.buttonOutlineText} >Register</Text>
                     </TouchableOpacity>
                 </View>
             </KeyboardAvoidingView>
@@ -47,5 +47,42 @@ export const LoginScreen = ({navigation}) => {
 }
 
 const loginstyles = StyleSheet.create({
-
+    inputContainer: {
+        width: '80%', 
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius:10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: '60%', //probably need to adjust this
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    button: {
+        backgroundColor: "blue",
+        width: "100%",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center"
+    },
+    buttonText:{
+        color: "white",
+        fontWeight: "700",
+        fontSize: 16
+    },
+    buttonOutline: {
+        backgroundColor: "white",
+        marginTop: 5,
+        borderColor: "blue",
+    },
+    buttonOutlineText: {
+        color: "blue",
+        fontWeight: "700",
+        fontSize: 16
+    },
 })
