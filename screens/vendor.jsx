d88c160b74160eeb3b627879567997bf531dc583
Clarice Kim <claricek@uchicago.edu>
2023-07-28 17:32:41 -0400

    vendor screen u pdates based on Database

--- screens/vendor.jsx
+++ screens/vendor.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, ScrollView, Pressable, StyleSheet, SafeAreaView, View, Text, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider, Flex} from "native-base";
-
+import { doc, getDoc, getDocs, collection, addDoc} from "firebase/firestore";
+import {db} from '../firebaseConfig';
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -10,10 +11,38 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 import styles from '../style.js'
 
 export const VendorScreen = ({navigation}) => {
+
+    const [vendorInfo, setVendorInfo] = useState("");
+    const [menu, setMenu] = useState([]);
+    const vendorDocRef = doc(db, "vendor", "5Ln5vKIXNa7ZNshoMDTQ");
+    const menuCollectionRef = collection(vendorDocRef, "Menu");
+
+    useEffect(() => {
+        const getVendor = async () => {
+            const docSnap = await getDoc(vendorDocRef);
+
+            if (docSnap.exists()) {
+                // console.log("Document data:", docSnap.data());
+                setVendorInfo(docSnap.data());
+                
+            } else {
+                // docSnap.data() will be undefined in this case
+                console.log("No such document!");
+                setVendorInfo()
+            }
+        };
+        getVendor();
+        const getMenu = async () => {
+            const menuData = await getDocs(menuCollectionRef);
+            setMenu(menuData.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+            console.log(menu)
+        }
+        getMenu();
+    }, []);
+      
     return (
 
         <SafeAreaView style={styles.layout}>
-            
             <View style={styles.header}>
                 <Text style={{fontSize: 30, fontWeight: "bold", textAlign: "center", marginLeft: 110}}>[App Name]</Text>
                 <Pressable onPress={() => navigation.navigate('CartScreen')}>
@@ -21,7 +50,10 @@ export const VendorScreen = ({navigation}) => {
                 </Pressable>
             </View>
 
-            
+            {/* <Pressable onPress={getVendor}>
+                <Text>get vendor</Text>
+            </Pressable> */}
+
             <ScrollView width={screenWidth} showsVerticalScrollIndicator={false}>
                 <View style={styles.container}>
                 <Image source={require("../assets/images/cucumber.jpg")} 
@@ -35,48 +67,40 @@ export const VendorScreen = ({navigation}) => {
 
                 <View style={{flexDirection:'row'}}>
                      <View style={itemstyles.container}>
-                     <Text style={styles.heading}>[Vendor]</Text>
+                     <Text style={styles.heading}>{vendorInfo.name}</Text>
                     </View>
 
                     <View style={itemstyles.container}>
-                     <Text style={[styles.bodytext, {textAlign: 'right'}]}>[Cost], [Location]</Text>
+                     <Text style={[styles.bodytext, {textAlign: 'right'}]}>{vendorInfo.location}</Text>
                      </View>
                 </View>
                 <View style={itemstyles.container}>
                      <Text style={styles.subheading}>Information:</Text>
-                     <Text style={styles.bodytext}>[Vendor Description]</Text>
+                     <Text style={styles.bodytext}>{vendorInfo.description}</Text>
                  </View>
                  </View>
                  {/* contact info, to come*/}
                  {/* MENU */}
                 <Text style={[styles.heading, {textAlign: 'center'}]} onPress={() => navigation.navigate('ItemScreen')}>Menu</Text>
                     <View>
-                     <Pressable style={itemstyles.pressable} onPress={() => navigation.navigate('ItemScreen')}>
+
+                    {menu.map((item) => {
+                        return(
+                        <View>
+                        <Pressable style={itemstyles.pressable} onPress={() => navigation.navigate('ItemScreen')}>
                         
-                         <Image source={require("../assets/images/banchan.jpg")} 
-                                alt="banchan1"
-                                w="full"
-                                h={24}
-                                resizeMode="contain"/>
-                         <Text>Item Name</Text>
-                        
-                     </Pressable>
-                     <Pressable style={itemstyles.pressable} onPress={() => navigation.navigate('ItemScreen')}>
-                         <Image source={require("../assets/images/banchan.jpg")} 
-                                alt="banchan1"
-                                w="full"
-                                h={24}
-                                resizeMode="contain"/>
-                         <Text>Item Name</Text>
-                     </Pressable>
-                     <Pressable style={itemstyles.pressable} onPress={() => navigation.navigate('ItemScreen')}>
-                         <Image source={require("../assets/images/banchan.jpg")} 
-                                alt="banchan1"
-                                width="100%"
-                                h={24}
-                                resizeMode="contain"/>
-                         <Text>Item Name</Text>
-                     </Pressable>
+                        <Image source={require("../assets/images/banchan.jpg")} 
+                               alt="banchan1"
+                               w="full"
+                               h={24}
+                               resizeMode="contain"/>
+                        <Text>{item.name}</Text>
+                        </Pressable>
+                        </View>
+                        );
+                   
+                    })}
+                    
                     </View>
 
             </ScrollView>
