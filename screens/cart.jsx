cf2353708421c25d7426d404a973b0502f3ddfe2
Catherine <catherinelu2004@gmail.com>
2023-07-31 00:22:57 -0500

    simple display items from database in cart + option to delete item from cart

--- screens/cart.jsx
+++ screens/cart.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import { db } from '../firebaseConfig';
 import { NavigationContainer } from '@react-navigation/native';
-import { ScrollView, Text, View, Image, Button, Pressable} from "react-native";
+import { ScrollView, Text, View, Image, Button, Pressable, StyleSheet, SafeAreaView} from "react-native";
 import { useEffect } from 'react';
-import { doc, addDoc, getDocs, collection } from 'firebase/firestore'; 
+import { doc, addDoc, getDocs, collection, deleteDoc } from 'firebase/firestore'; 
 import { isReactNative } from '@firebase/util';
 import { auth } from '../firebaseConfig.js';
 
@@ -11,6 +11,7 @@ import { auth } from '../firebaseConfig.js';
 export const CartScreen = ({navigation}) => {
 
     // //have a use state variable for the collection
+    const [cart, setCart] = useState([]);
 
     // accesses document for specific user - we need to replace this with a variable for the user signed in
     const usersDocRef = doc(db, "users", auth.currentUser.uid)
@@ -20,7 +21,24 @@ export const CartScreen = ({navigation}) => {
         await //addDoc(usersCollectionRef, { email: "email", password: "password" })
         addDoc(cartCollectionRef, {name: "sample", quantity: 0, note: ""})
     };
-    
+
+    const deleteItem = async(id) => {
+        const itemDoc = doc(usersDocRef, "cart", id);
+        await deleteDoc(itemDoc)
+        navigation.replace("CartScreen")
+        console.log("deleted")
+    }
+
+    useEffect(() => {
+        const getCart = async() => {
+            const data = await getDocs(cartCollectionRef);
+            setCart(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+        };
+
+        getCart();
+    }, []);
+
+
     
     // useEffect(() => {
     //     //function to access data through use effect async function
@@ -42,23 +60,37 @@ export const CartScreen = ({navigation}) => {
     
     
     return (
-        <View>
-            <Text>Cart</Text>
-            <Pressable onPress={createUser}>
-                <Text>add</Text>
-            </Pressable>
-            {/* <View>
-               
-                {user.map((oneuser) => { 
-                    return (
-                    <View>
-                        
-                        <Text>Item 1: {oneuser.cart[0]}</Text>
-                    </View>
-                    );
+        <SafeAreaView style={styles.layout}>
+            <View style={styles.header}>
+                <Text style={{fontSize: 30, fontWeight: "bold", textAlign: "center"}}>Cart</Text>
+            </View>
 
-                })}
-            </View> */}
-        </View>
+
+            {cart.map((item) => {
+                return <View> 
+                    <Text>Item: {item.name}</Text>
+                    <Text>Quantity: {item.quantity}</Text>
+                    <Button title="Delete" onPress={() => {deleteItem(item.id)}}></Button>
+                </View>;
+        })}
+        
+
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        width: "100%",
+        height: 75,
+        backgroundColor: "#FFF9A6",
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: "row",
+    },
+    layout: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+  })
