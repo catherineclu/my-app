bfebf4920e33d2cf31ddff2f8ef33aaa07d94346
Catherine <catherinelu2004@gmail.com>
2023-08-27 01:37:25 +0800

    card info collection added to checkout screen, still can't access network

--- screens/checkout.jsx
+++ screens/checkout.jsx
@@ -1,77 +1,76 @@
 import React from "react"
-import { Text, View, Button, SafeAreaView } from "react-native";
-import { useStripe } from '@stripe/stripe-react-native';
-import { useState, useEffect } from "react";
-
-
+import { View, Button } from "react-native";
+import { CardField, useStripe, useConfirmPayment } from '@stripe/stripe-react-native';
 
 export const CheckoutScreen = ({navigation}) => {
-    const { initPaymentSheet, presentPaymentSheet } = useStripe();
-    const [loading, setLoading] = useState(false);
-  
-    const fetchPaymentSheetParams = async () => {
-      const response = await fetch(`${API_URL}/payment-sheet`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const { paymentIntent, ephemeralKey, customer} = await response.json();
-  
-      return {
-        paymentIntent,
-        ephemeralKey,
-        customer,
-      };
-    };
-  
-    const initializePaymentSheet = async () => {
-      const {
-        paymentIntent,
-        ephemeralKey,
-        customer,
-        publishableKey,
-      } = await fetchPaymentSheetParams();
-  
-      const { error } = await initPaymentSheet({
-        merchantDisplayName: "Example, Inc.",
-        customerId: customer,
-        customerEphemeralKeySecret: ephemeralKey,
-        paymentIntentClientSecret: paymentIntent,
-        // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
-        //methods that complete payment after a delay, like SEPA Debit and Sofort.
-        allowsDelayedPaymentMethods: true,
-        defaultBillingDetails: {
-          name: 'Jane Doe',
-        }
-      });
-      if (!error) {
-        setLoading(true);
-      }
-    };
-  
-    const openPaymentSheet = async () => {
-        const { error } = await presentPaymentSheet();
+    const {confirmPayment, loading} = useConfirmPayment();
+    // ...
 
+        
+    const API_URL = 'server.js'
+    const fetchPaymentIntentClientSecret = async () => {
+        const response = await fetch(`${API_URL}/create-payment-intent`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            currency: 'usd',
+          }),
+        });
+        const {clientSecret} = await response.json();
+    
+        return clientSecret;
+      };
+    
+      const handlePayPress = async () => {
+        // Gather the customer's billing information (for example, email)
+        const billingDetails = {
+          email: 'jenny.rosen@example.com',
+        };
+    
+        // Fetch the intent client secret from the backend
+        const clientSecret = await fetchPaymentIntentClientSecret();
+    
+        // Confirm the payment with the card details
+        const {paymentIntent, error} = await confirmPayment(clientSecret, {
+          paymentMethodType: 'Card',
+          paymentMethodData: {
+            billingDetails,
+          },
+        });
+    
         if (error) {
-          Alert.alert(`Error code: ${error.code}`, error.message);
-        } else {
-          Alert.alert('Success', 'Your order is confirmed!');
+          console.log('Payment confirmation error', error);
+        } else if (paymentIntent) {
+          console.log('Success from promise', paymentIntent);
         }
-    };
-  
-    useEffect(() => {
-      initializePaymentSheet();
-    }, []);
-  
-    return (
-      <View>
-        <Button
-          variant="primary"
-          disabled={!loading}
-          title="Checkout"
-          onPress={openPaymentSheet}
-        />
-      </View>
-    );
-  }
+      };
+
+  return (
+    <View>
+      <CardField
+        postalCodeEnabled={true}
+        placeholders={{
+          number: '4242 4242 4242 4242',
+        }}
+        cardStyle={{
+          backgroundColor: '#FFFFFF',
+          textColor: '#000000',
+        }}
+        style={{
+          width: '100%',
+          height: 50,
+          marginVertical: 30,
+        }}
+        onCardChange={(cardDetails) => {
+          console.log('cardDetails', cardDetails);
+        }}
+        onFocus={(focusedField) => {
+          console.log('focusField', focusedField);
+        }}
+      />
+        <Button onPress={handlePayPress} title="Pay"/>
+    </View>
+  );
+}
--- server.js
+++ server.js
@@ -1,27 +1,11 @@
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
 const stripe = require('stripe')('sk_test_51NcuFEGCn9Upn7WMgjyVotXKDo2gNM0v7mQ55AH6cRwrctNWM0C6Tcqm6z0BT9IG3gzbbP6hdlhkWlDrhgeDxHeI00oUO84DkR');
-// This example sets up an endpoint using the Express framework.
-// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
 
-app.post('/payment-sheet', async (req, res) => {
-  // Use an existing Customer ID if this is a returning customer.
-  const customer = await stripe.customers.create();
-  const ephemeralKey = await stripe.ephemeralKeys.create(
-    {customer: customer.id},
-    {apiVersion: '2022-11-15'}
-  );
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'eur',
-    customer: customer.id,
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-
-  res.json({
-    paymentIntent: paymentIntent.client_secret,
-    ephemeralKey: ephemeralKey.secret,
-    customer: customer.id,
-    publishableKey: 'pk_test_51NcuFEGCn9Upn7WMRkokFnh1gXtJigvFeeRqUwwmSDWmXGDq4tzsbdveQb5jOnauAFi4F7hwoHvSDjQhyPOxhUdw00d3EqDDL6'
-  });
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: 1099,
+  currency: 'usd',
+  payment_method_types: ['card'],
 });
+const clientSecret = paymentIntent.client_secret
+// Pass the client secret to the client
